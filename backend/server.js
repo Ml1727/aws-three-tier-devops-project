c0cc68d80fc559bc3f906fbcd467dbdd6b7fbf07
Ml1727 <gsishir2004@gmail.com>
2026-06-17 06:52:49 +0000

    Add environment variable support using dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
 const express = require("express");
+require("dotenv").config();
+
+const cors = require("cors");
+
 const healthRoutes = require("./routes/health");
 const userRoutes = require("./routes/users");
 const productRoutes = require("./routes/products");
 const orderRoutes = require("./routes/orders");
-const cors = require("cors");
-
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use("/health", healthRoutes);
 app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("AWS Three-Tier Backend is running");
 });
 
-
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
